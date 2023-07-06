4a1227258936f6ecc8ec86c99b242a104693d11b
RunningLiLi <3238367573@qq.com>
2023-07-06 09:28:45 +0800

    fix: import带盘符的绝对路径bug

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="App-logo" src={logo} alt="" />
+        {/* <img className="App-logo" src={logo} alt="" /> */}
         <p>Hello Vite + React</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
